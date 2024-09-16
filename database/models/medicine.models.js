fb5d4de5d89a279e6932b05f0b12e34f4720a38d
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-09-16 14:53:49 +0300

    docs: create medicine.models.js file

---
+++ database/models/medicine.models.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const MedicineSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: [true, "Name is required"],
+      maxlength: [100, "Name should not exceed 100 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Quantity is required"],
+      min: [0, "Quantity cannot be negative"],
+    },
+    dosage: {
+      type: String,
+      maxlength: [50, "Dosage should not exceed 50 characters"],
+    },
+    description: {
+      type: String,
+    },
+    imageUrl: {
+      type: String,
+      maxlength: [255, "Image URL should not exceed 255 characters"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const Medicine = mongoose.model("Medicine", MedicineSchema);
