73fe84efd6250b930032221bf02e8b0cb83a854f
0xmuka <hemasallam03@gmail.com>
2024-09-16 15:09:38 +0300

    add review module

---
+++ database/models/Review.js
@@ -0,0 +1,25 @@
+import mongoose, { Types } from 'mongoose';
+
+const reviewSchema = new Schema({
+  comment: {
+    type: String,
+    required: false,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
