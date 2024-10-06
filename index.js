857e900afff46c3d3de6e963cf83b72f5c1043ac
ayman <aymanshalaby539@gmail.com>
2024-10-06 15:55:21 +0300

    feat: creat dockerfile

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
-// import { dbConn } from './database/dbConnection.js'
+//* import { dbConn } from './database/dbConnection.js'*
 const app = express();
+
+
+
+
+
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
